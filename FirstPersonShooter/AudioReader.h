c1a5b64d956834942684fe1fd3706c3e14873e2c
Adam Zylinski <adamowski137@gmail.com>
2024-10-29 11:04:26 +0100

    wip

--- FirstPersonShooter/AudioReader.h
+++ FirstPersonShooter/AudioReader.h
@@ -9,4 +9,5 @@ public:
 
 private:
 	static HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunkDataPosition);
+	static HRESULT ReadChunkData(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset);
 };
