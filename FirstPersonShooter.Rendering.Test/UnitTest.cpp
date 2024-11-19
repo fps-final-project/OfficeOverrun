9f01bdc773f30c8eed3924b31ec403c9d357a4ed
ingeniousname <miloszwozny5@gmail.com>
2024-11-19 16:57:47 +0100

    added rendering test project

---
+++ FirstPersonShooter.Rendering.Test/UnitTest.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "Mesh.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FirstPersonShooter_Rendering_Test
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            Mesh mesh;
+            Assert::AreEqual(1, 1);
+            // TODO: Your test code here
+        }
+    };
+}
