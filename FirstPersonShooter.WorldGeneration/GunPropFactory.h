f7c759ae1b500ee813909d47d3d26a8120c907f8
StiffGitt <131384247+StiffGitt@users.noreply.github.com>
2025-01-02 21:24:56 +0100

    GunProps refactor

--- FirstPersonShooter.WorldGeneration/GunPropFactory.h
+++ FirstPersonShooter.WorldGeneration/GunPropFactory.h
@@ -4,11 +4,11 @@
 
 namespace WorldGenerator
 {
-	class GunPropInstanceFactory
+	class GunPropFactory
 	{
 	private:
 		static constexpr DirectX::XMFLOAT3 GUN_PROP_SIZE = DirectX::XMFLOAT3(0.5f, 0.5f, 0.5f);
 	public:
-		static GunPropInstance* CreateGunPropInstance(GunType gun_type, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation);
+		static GunProp* CreateGunProp(GunType gun_type);
 	};
 }
--- FirstPersonShooter.WorldGeneration/GunPropInstance.h
+++ FirstPersonShooter.WorldGeneration/GunPropInstance.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PropInstance.hpp"
+#include "GunProp.h"
 
 namespace WorldGenerator
 {
@@ -8,10 +9,10 @@ namespace WorldGenerator
 	{
 		std::string model_name;
 
-		GunPropInstance(const std::string& model_name, const std::string& name, 
-			const DirectX::XMFLOAT3& position, DirectX::XMFLOAT3 size, const DirectX::XMFLOAT3& rotation, bool isSolid)
-			: model_name(model_name), PropInstance(name, position, size, rotation, isSolid)
+		GunPropInstance(GunProp prop, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation)
+			: model_name(prop.model_name), PropInstance(prop, position, rotation)
 		{
+
 		}
 	};
 }
